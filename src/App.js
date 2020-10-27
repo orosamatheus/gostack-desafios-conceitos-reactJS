3af265bfdbba49767b7f0fc748881f2a5fc98f15
Matheus Rosa <matheusrosa.la@gmail.com>
2020-10-27 19:54:11 -0300

    Configurando função de listagem de repositórios

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const[repositories, setRepositories] = useState([]);
 
-
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    });
+  },[]);
 
   async function handleAddRepository() {
     // TODO
@@ -17,13 +22,12 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+          {repositories.map(repository => <li key = {repository.id}>{repository.title}</li>)}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
-        </li>
+       
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
